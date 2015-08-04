cae51622e8616e92004907d93311d5dc03d37229
Jerrod Junker <unsafepond@gmail.com>
2015-08-04 15:59:10 -0600

    fixed typo

--- lib/transaction-repo.rb
+++ lib/transaction-repo.rb
@@ -8,7 +8,7 @@ class TransactionRepository
 
   def make_transactions
     transactions.by_row.map do |row|
-      Transaction.new(row[:id], row[:invoice_id], row[:credit_card_number], row[:credit_card_experation_date], row[:result], row[:created_at], row[:updated_at])
+      Transaction.new(row[:id], row[:invoice_id], row[:credit_card_number], row[:credit_card_expiration_date], row[:result], row[:created_at], row[:updated_at])
     end
   end
 end
