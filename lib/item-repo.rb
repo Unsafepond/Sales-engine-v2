b29e9439d7a1052b3788434a9b48b4a77eb7b64f
Michael Wong <michael.brian.wong@gmail.com>
2015-08-05 11:48:45 -0600

    added find by methods for item repos

--- lib/item-repo.rb
+++ lib/item-repo.rb
@@ -1,9 +1,10 @@
 require_relative 'item'
 class ItemRepository
-  attr_reader :item
+  attr_reader :item, :all_items
 
   def initialize(csvtable)
     @item = csvtable
+    @all_items = make_items
   end
 
   def make_items
@@ -16,5 +17,69 @@ class ItemRepository
                row[:created_at],
                row[:updated_at])
     end
+  end
+
+  def all
+    all_items
+  end
+
+  def random
+    all.sample
+  end
+
+  def find_by_id(id)
+    all.detect {|item| item.id == id}
+  end
+
+  def find_by_name(name)
+    all.detect {|item| item.name == name}
+  end
+
+  def find_by_description(description)
+    all.detect {|item| item.description == description}
+  end
+
+  def find_by_unit_price(unit_price)
+    all.detect {|item| item.unit_price == unit_price}
+  end
+
+  def find_by_merchant_id(merchant_id)
+    all.detect {|item| item.merchant_id == merchant_id}
+  end
+
+  def find_by_created_at(created_date)
+    all.detect {|item| item.created_at == created_date}
+  end
+
+  def find_by_updated_at(updated_date)
+    all.detect {|item| item.updated_at == updated_date}
+  end
+
+  def find_all_by_id(id)
+    all.select {|item| item.id == id}
+  end
+
+  def find_all_by_name(name)
+    all.select {|item| item.name == name}
+  end
+
+  def find_all_by_description(description)
+    all.select {|item| item.description == description}
+  end
+
+  def find_all_by_unit_price(unit_price)
+    all.select {|item| item.unit_price == unit_price}
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    all.select {|item| item.merchant_id == merchant_id}
+  end
+
+  def find_all_by_created_at(created_date)
+    all.select {|item| item.created_at == created_date}
+  end
+
+  def find_all_by_updated_at(updated_date)
+    all.select {|item| item.updated_at == updated_date}
   end
 end
--- test/item-repo-test.rb
+++ test/item-repo-test.rb
@@ -59,4 +59,130 @@ class TestItemRepository < Minitest::Test
     item_repo = ItemRepository.new(data)
     assert_equal "2012-03-27 14:53:59 UTC", item_repo.make_items.last.updated_at
   end
+
+  def test_all_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal '1', item_repo.all.first.id
+  end
+
+  def test_random_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal Item, item_repo.random.class
+  end
+
+  def test_find_by_id_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "Item Qui Esse", item_repo.find_by_id("1").name
+  end
+
+  def test_find_by_name_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "2",
+    item_repo.find_by_name("Item Autem Minima").id
+  end
+
+  def test_find_by_description_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "2",
+    item_repo.find_by_description("Cumque consequuntur ad.").id
+  end
+
+  def test_find_by_unit_price
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "2",
+    item_repo.find_by_unit_price("67076").id
+  end
+
+  def test_find_by_merchant_id_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_by_merchant_id("1").id
+  end
+
+  def test_find_by_created_at_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_by_created_at("2012-03-27 14:53:59 UTC").id
+  end
+
+  def test_find_by_updated_at_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_by_updated_at("2012-03-27 14:53:59 UTC").id
+  end
+
+  def test_find_all_by_id_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "Item Qui Esse",
+    item_repo.find_all_by_id("1").first.name
+  end
+
+  def test_find_all_by_name_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "3",
+    item_repo.find_all_by_name("Item Ea Voluptatum").first.id
+  end
+
+  def test_find_all_by_description_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "3",
+    item_repo.find_all_by_description("Sunt officia eum qui molestiae.").first.id
+  end
+
+  def test_find_all_by_unit_price_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_all_by_unit_price("75107").first.id
+  end
+
+  def test_find_all_by_merchant_id_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_all_by_merchant_id("1").first.id
+  end
+
+  def test_find_all_by_created_at_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_all_by_created_at("2012-03-27 14:53:59 UTC").first.id
+  end
+
+  def test_find_all_by_updated_at_method
+    data = CSV.read "./data/fixtures/items.csv",
+    headers: true, header_converters: :symbol
+    item_repo = ItemRepository.new(data)
+    assert_equal "1",
+    item_repo.find_all_by_updated_at("2012-03-27 14:53:59 UTC").first.id
+  end
+
 end
