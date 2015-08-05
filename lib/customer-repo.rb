8698347b75d38d333e94ceb1d0c1510b207f41da
Michael Wong <michael.brian.wong@gmail.com>
2015-08-05 12:03:44 -0600

    added find by methods for customer repo

--- lib/customer-repo.rb
+++ lib/customer-repo.rb
@@ -1,9 +1,10 @@
 require_relative 'customer'
 class CustomerRepository
-  attr_reader :customers
+  attr_reader :customers, :all_customers
 
   def initialize(csvtable)
     @customers = csvtable
+    @all_customers = make_customers
   end
 
   def make_customers
@@ -14,5 +15,53 @@ class CustomerRepository
                    row[:created_at],
                    row[:updated_at])
     end
+  end
+
+  def all
+    all_customers
+  end
+
+  def random
+    all.sample
+  end
+
+  def find_by_id(id)
+    all.detect {|customer| customer.id == id}
+  end
+
+  def find_by_first_name(first_name)
+    all.detect {|customer| customer.first_name == first_name}
+  end
+
+  def find_by_last_name(last_name)
+    all.detect {|customer| customer.last_name == last_name}
+  end
+
+  def find_by_created_at(created_date)
+    all.detect {|customer| customer.created_at == created_date}
+  end
+
+  def find_by_updated_at(updated_date)
+    all.detect {|customer| customer.updated_at == updated_date}
+  end
+
+  def find_all_by_id(id)
+    all.select {|customer| customer.id == id}
+  end
+
+  def find_all_by_first_name(first_name)
+    all.select {|customer| customer.first_name == first_name}
+  end
+
+  def find_all_by_last_name(last_name)
+    all.select {|customer| customer.last_name == last_name}
+  end
+
+  def find_all_by_created_at(created_date)
+    all.select {|customer| customer.created_at == created_date}
+  end
+
+  def find_all_by_updated_at(updated_date)
+    all.select {|customer| customer.updated_at == updated_date}
   end
 end
--- test/customer-repo-test.rb
+++ test/customer-repo-test.rb
@@ -36,13 +36,109 @@ class TestCustomerRepository < Minitest::Test
     data = CSV.read "./data/fixtures/customers.csv",
     headers: true, header_converters: :symbol
     customer_repo = CustomerRepository.new(data)
-    assert_equal "2012-03-27 14:54:10 UTC", customer_repo.make_customers.last.created_at
+    assert_equal "2012-03-27 14:54:10 UTC",
+    customer_repo.make_customers.last.created_at
   end
 
   def test_we_can_make_customer_update_time_with_table
     data = CSV.read "./data/fixtures/customers.csv",
     headers: true, header_converters: :symbol
     customer_repo = CustomerRepository.new(data)
-    assert_equal "2012-03-27 14:54:10 UTC", customer_repo.make_customers.last.updated_at
+    assert_equal "2012-03-27 14:54:10 UTC",
+    customer_repo.make_customers.last.updated_at
   end
+
+  def test_all_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal '1', customer_repo.all.first.id
+  end
+
+  def test_random_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal Customer, customer_repo.random.class
+  end
+
+  def test_find_by_id_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "Joey", customer_repo.find_by_id("1").first_name
+  end
+
+  def test_find_by_first_name_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_by_first_name("Joey").id
+  end
+
+  def test_find_by_last_name_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_by_last_name("Ondricka").id
+  end
+
+  def test_find_by_created_at_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_by_created_at("2012-03-27 14:54:09 UTC").id
+  end
+
+  def test_find_by_updated_at_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_by_updated_at("2012-03-27 14:54:09 UTC").id
+  end
+
+  def test_find_all_by_id_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "Joey",
+    customer_repo.find_all_by_id("1").first.first_name
+  end
+
+  def test_find_all_by_first_name_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_all_by_first_name("Joey").first.id
+  end
+
+  def test_find_all_by_last_name_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_all_by_last_name("Ondricka").first.id
+  end
+
+  def test_find_all_by_created_at_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_all_by_created_at("2012-03-27 14:54:09 UTC").first.id
+  end
+
+  def test_find_all_by_updated_at_method
+    data = CSV.read "./data/fixtures/customers.csv",
+    headers: true, header_converters: :symbol
+    customer_repo = CustomerRepository.new(data)
+    assert_equal "1",
+    customer_repo.find_all_by_updated_at("2012-03-27 14:54:09 UTC").first.id
+  end
+
 end
