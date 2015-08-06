24fbcfa365259e8c0e98704804132b194757290a
Jerrod Junker <unsafepond@gmail.com>
2015-08-06 09:49:48 -0600

    finished transaction relationship

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,7 +5,8 @@ class Transaction
               :credit_card_expiration_date,
               :result,
               :created_at,
-              :updated_at
+              :updated_at,
+              :transaction_repo
 
   def initialize(id,
                  invoice_id,
@@ -13,7 +14,8 @@ class Transaction
                  credit_card_experation_date,
                  result,
                  created_at,
-                 updated_at)
+                 updated_at,
+                 transaction_repo = "")
     @id = id
     @invoice_id = invoice_id
     @credit_card_number = credit_card_number
@@ -21,5 +23,10 @@ class Transaction
     @result = result
     @created_at = created_at
     @updated_at = updated_at
+    @transaction_repo = transaction_repo
+  end
+
+  def invoice
+    transaction_repo.sales_engine.invoice_repository.find_by_id(invoice_id)
   end
 end
--- test/transaction-test.rb
+++ test/transaction-test.rb
@@ -1,6 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/transaction'
+require_relative "test_helper"
 
 class TestTransaction < Minitest::Test
   def test_transaction_has_id
@@ -18,9 +16,9 @@ class TestTransaction < Minitest::Test
     assert_equal "535", transaction.invoice_id
   end
 
-  def test_transaction_has_credit_card_experation_date
+  def test_transaction_has_credit_card_expiration_date
     transaction = Transaction.new("4", "535", "1", "3", "2196", "2012-03-27 14:54:09 UTC", "2012-03-27 14:54:09 UTC")
-    assert_equal "3", transaction.credit_card_experation_date
+    assert_equal nil, transaction.credit_card_expiration_date
   end
 
   def test_transaction_has_result
@@ -36,5 +34,13 @@ class TestTransaction < Minitest::Test
   def test_transaction_has_updated_date
     transaction = Transaction.new("4", "535", "1", "3", "2196", "2012-03-27 14:54:09 UTC", "2012-03-27 14:54:09 UTC")
     assert_equal "2012-03-27 14:54:09 UTC", transaction.updated_at
+  end
+
+  def test_invocie_method
+    engine = SalesEngine.new("./data/fixtures")
+    engine.startup
+    t_repo = engine.transaction_repository
+    transaction = Transaction.new("4", "1", "1", "3", "2196", "2012-03-27 14:54:09 UTC", "2012-03-27 14:54:09 UTC", t_repo)
+    assert_equal Invoice, transaction.invoice.class
   end
 end
